aec502d5deb612bcf097337e92d0c16fad22ef5e
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2020-08-05 07:39:15 +0900

    feat: Apply global styles

---
+++ global-style.tsx
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+
+  html, body {
+    margin: 0;
+    padding: 0;
+    overscroll-behavior: none; /* https://www.smashingmagazine.com/2018/08/scroll-bouncing-websites/ */
+  }
+
+  a, abbr, acronym, address, applet, article, aside, audio, b, big, blockquote, body, button, canvas, caption, center, cite, code, dd, del, details, dfn, div, dl, dt, em, embed, fieldset, figcaption, figure, footer, form, h1, h2, h3, h4, h5, h6, header, hgroup, html, i, iframe, img, input, ins, kbd, label, legend, li, mark, menu, nav, object, ol, output, p, pre, q, ruby, s, samp, section, select, small, span, strike, strong, sub, summary, sup, table, tbody, td, textarea, tfoot, th, thead, time, tr, tt, u, ul, var, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font: inherit;
+    vertical-align: inherit;
+    outline: none;
+    letter-spacing: inherit;
+    text-decoration: none;
+    cursor: inherit;
+  }
+`;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import App from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../theme';
+import { GlobalStyle } from '../global-style';
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     );
