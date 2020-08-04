ce578efa6bc2a65c6dc064aeee2c5f6fcc7368d3
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2020-08-04 21:29:33 +0900

    feat: Init styled-components

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import App from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../theme';
+
+export default class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    );
+  }
+}
---
+++ theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  colors: {
+    mint: '#3EB489',
+    grey: '#b3b3b3',
+  },
+  fonts: {
+    lato: 'Lato',
+  },
+};
