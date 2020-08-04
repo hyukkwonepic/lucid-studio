699267019921c768187204d98f003f1f90e10750
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2020-08-04 20:32:43 +0900

    feat: Add dev settings

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2020: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/react-in-jsx-scope": 0,
+    "@typescript-eslint/explicit-module-boundary-types": 0,
+  },
+};
