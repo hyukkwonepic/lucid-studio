be99bd7b4eb67a14ffbe0e991cb70da80a12a5eb
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2020-08-24 16:36:43 +0900

    feat: Add selection box for rectangle

---
+++ components/canvas/page/selection-layer/rectangle-selection-box/border-bottom/border-bottom.tsx
@@ -0,0 +1,89 @@
+import { useEditor } from '../../../../../../hooks/useEditor';
+import { usePage } from '../../../../../../hooks/usePage';
+import { useRectangle, RectangleState } from '../../../../../../hooks/useRectangle';
+import { useState, useEffect, FC, MouseEvent } from 'react';
+import { RecoilState } from '../../../../../../services/recoil-unstable';
+
+const BorderBottom: FC<{
+  rectangleState: RecoilState<RectangleState>;
+}> = ({ rectangleState }) => {
+  const editor = useEditor();
+  const page = usePage(editor.selectedPage);
+  const rectangle = useRectangle(rectangleState);
+  const [state, setState] = useState({
+    down: false,
+    rectangle: {
+      x: 0,
+      y: 0,
+      width: 0,
+      height: 0,
+    },
+  });
+
+  useEffect(() => {
+    const handleMouseMove = (event: globalThis.MouseEvent) => {
+      const rect = page.ref.current.getBoundingClientRect();
+      const y = event.clientY - rect.top;
+
+      const newHeight = y - rectangle.y;
+
+      if (newHeight > 0) {
+        rectangle.resize(rectangle.width, newHeight);
+      } else {
+        rectangle.resize(rectangle.width, 1);
+      }
+    };
+
+    if (state.down) {
+      window.addEventListener('mousemove', handleMouseMove);
+    }
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, [state, rectangle, page.ref]);
+
+  useEffect(() => {
+    const handleMouseUp = () => {
+      setState((state) => ({ ...state, down: false }));
+    };
+
+    if (state.down) {
+      window.addEventListener('mouseup', handleMouseUp);
+    }
+
+    return () => {
+      window.removeEventListener('mouseup', handleMouseUp);
+    };
+  }, [state]);
+
+  const handleMouseDown = (event: MouseEvent<HTMLDivElement, globalThis.MouseEvent>) => {
+    event.stopPropagation();
+    setState({
+      down: true,
+      rectangle: {
+        x: rectangle.x,
+        y: rectangle.y,
+        width: rectangle.width,
+        height: rectangle.height,
+      },
+    });
+  };
+
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        left: rectangle.x - 2,
+        top: rectangle.y + rectangle.height,
+        width: rectangle.width + 2,
+        height: '2px',
+        backgroundColor: '#51BC95',
+        cursor: 'ns-resize',
+      }}
+      onMouseDown={handleMouseDown}
+    />
+  );
+};
+
+export default BorderBottom;
---
+++ components/canvas/page/selection-layer/rectangle-selection-box/border-top/border-top.tsx
@@ -0,0 +1,91 @@
+import { useEditor } from '../../../../../../hooks/useEditor';
+import { usePage } from '../../../../../../hooks/usePage';
+import { useRectangle, RectangleState } from '../../../../../../hooks/useRectangle';
+import { useState, useEffect, FC, MouseEvent } from 'react';
+import { RecoilState } from '../../../../../../services/recoil-unstable';
+
+const BorderTop: FC<{
+  rectangleState: RecoilState<RectangleState>;
+}> = ({ rectangleState }) => {
+  const editor = useEditor();
+  const page = usePage(editor.selectedPage);
+  const rectangle = useRectangle(rectangleState);
+  const [state, setState] = useState({
+    down: false,
+    rectangle: {
+      x: 0,
+      y: 0,
+      width: 0,
+      height: 0,
+    },
+  });
+
+  useEffect(() => {
+    const handleMouseMove = (event: globalThis.MouseEvent) => {
+      const rect = page.ref.current.getBoundingClientRect();
+      const y = event.clientY - rect.top;
+
+      const newHeight = state.rectangle.height + state.rectangle.y - y;
+
+      if (newHeight > 0) {
+        rectangle.moveTo(rectangle.x, y);
+        rectangle.resize(rectangle.width, newHeight);
+      } else {
+        rectangle.moveTo(rectangle.x, state.rectangle.y + state.rectangle.height - 1);
+        rectangle.resize(rectangle.width, 1);
+      }
+    };
+
+    if (state.down) {
+      window.addEventListener('mousemove', handleMouseMove);
+    }
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, [state, rectangle, page.ref]);
+
+  useEffect(() => {
+    const handleMouseUp = () => {
+      setState((state) => ({ ...state, down: false }));
+    };
+
+    if (state.down) {
+      window.addEventListener('mouseup', handleMouseUp);
+    }
+
+    return () => {
+      window.removeEventListener('mouseup', handleMouseUp);
+    };
+  }, [state]);
+
+  const handleMouseDown = (event: MouseEvent<HTMLDivElement, globalThis.MouseEvent>) => {
+    event.stopPropagation();
+    setState({
+      down: true,
+      rectangle: {
+        x: rectangle.x,
+        y: rectangle.y,
+        width: rectangle.width,
+        height: rectangle.height,
+      },
+    });
+  };
+
+  return (
+    <div
+      style={{
+        position: 'absolute',
+        left: rectangle.x,
+        top: rectangle.y - 2,
+        width: rectangle.width + 2,
+        height: '2px',
+        backgroundColor: '#51BC95',
+        cursor: 'ns-resize',
+      }}
+      onMouseDown={handleMouseDown}
+    />
+  );
+};
+
+export default BorderTop;
