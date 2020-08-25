4bfbf85a255f0802b06500f0868ae3f42518943f
Hyeokwoo Alex Kwon <khwalex@gmail.com>
2020-08-25 18:57:06 +0900

    fix: Resolve module import path bug

--- components/canvas/hover-layer/hover-layer.tsx
+++ components/canvas/hover-layer/hover-layer.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import { useEditor } from '../../../hooks/useEditor';
 import { usePage } from '../../../hooks/usePage';
-import { useRecoilValue, RecoilState } from '../../../services/recoil-unstable';
+import { useRecoilValue, RecoilState } from 'recoil';
 import { GraphicObjectState } from '../../../recoil/atoms';
 import RectangleHoverBox from './rectangle-hover-box/rectangle-hover-box';
 import { RectangleState } from '../../../hooks/useRectangle';
